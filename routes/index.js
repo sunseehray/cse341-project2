64f5c8eee010d8fa78257980cb448ddce14e872c
sunseehray <sabtirazona@byui.edu>
2023-09-27 15:42:08 +0800

    added api-docs

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router
-    // .use('/', require('./swagger'))
+    .use('/', require('./swagger'))
     .get('/', (req, res) => {
         //#swagger.tags=['Sunseehray Tirazona']
         res.send('Sunseehray Tirazona');
